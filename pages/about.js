e6388bb286dd9738fb79b84225201faa4d819e84
g-prog <alaobukky2@gmail.com>
2021-03-23 06:08:21 -0700

    subtle changes

--- pages/about.js
+++ pages/about.js
@@ -9,21 +9,22 @@ function about() {
   return (
     <div>
       <div className="bg-about-smile bg-cover min-h-screen p-20">
-        <div className="bg-blue-400 mt-40 rounded-full bg-opacity-50 p-20 float-left ml-0 w-1/2 h-96 pt-5">
+        <div className="bg-blue-400 mt-20 rounded-full bg-opacity-50 p-20 float-left ml-0 w-1/2 h-96 pt-5 ">
+        <div className="p-1 bg-blue-700 w-full bg-opacity-50 mt-11"></div>
           <section className="m-5 py-9 ">
-          <div className="p-1 bg-blue-700 w-full bg-opacity-50"></div>
-            <span className="text-4xl text-white  font-bold p-0 m-0">
+          
+            <span className="text-4xl text-white  font-bold">
               Africa's Blockchain Center for Developers
             </span>
-            <p className="text-lg text-white italic mt-6">
+            <p className="text-lg text-white italic">
               empowering the African woman for greater exploits...
             </p>
             
           </section>
           <div className="p-1 bg-blue-700 w-full bg-opacity-50 top-0"></div>
-          <section className="mx-5 my-10">
+          <section className="mx-5 my-10 mt-5">
             <Link href="/">
-              <a className="bg-blue-700 hover:bg-blue-100 text-white hover:text-blue-700 p-5 rounded-lg">
+              <a className="bg-blue-700 hover:bg-blue-100 text-white hover:text-blue-700 p-3  rounded-lg">
                 Learn more
               </a>
             </Link>
@@ -45,9 +46,9 @@ function about() {
                   <img class="h-48 w-full object-cover md:w-48" src="/table.jpg" alt="Man looking at item at a store"/>
               </div>
             <div class="p-8">
-              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-              <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Cardano Hub</div>
+              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">We at ABCD are committed to continuous and groundbreaking tech growth in Africa</a>
+              <p class="mt-2 text-gray-500">Women are the core of technological and we are all for Women empowerment.</p>
             </div>
           </div>
         </div>
@@ -59,9 +60,9 @@ function about() {
                   <img class="h-48 w-full object-cover md:w-48" src="/blue.jpg" alt="Man looking at item at a store"/>
               </div>
             <div class="p-8">
-              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-              <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Build Applications for the Blockchaon</div>
+              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">We at ABCD are committed to continuous and groundbreaking tech growth in Africa</a>
+              <p class="mt-2 text-gray-500">Women are the core of technological and we are all for Women empowerment.</p>
             </div>
           </div>
         </div>
@@ -73,9 +74,9 @@ function about() {
                   <img class="h-48 w-full object-cover md:w-48" src="/robot.jpg" alt="Man looking at item at a store"/>
               </div>
             <div class="p-8">
-              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-              <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+              <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Innovation</div>
+              <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">We at ABCD are committed to continuous and groundbreaking tech growth in Africa</a>
+              <p class="mt-2 text-gray-500">Women are the core of technological and we are all for Women empowerment.</p>
             </div>
           </div>
         </div>
@@ -92,9 +93,8 @@ function about() {
       <div className="bg-gradient-to-r from-blue-800 via-white to-blue-800  rounded-3xl flex flex-row  ">
         <div className="w-3/6  " >
         <img className="mt-32 ml-24 rounded-tl-lg rounded-tr-lg mb-10 bg-white m-0 auto " width="100%" height="100%" src="/robot.jpg" alt="Man looking at item at a store"/>
-        <div className="bg-white m-20 mt-96 mt-auto p-20 mt-px ">
-          
-        </div>
+        
+        
 
         </div>
         <div className=" w-3/6 ml-96 p-32">
